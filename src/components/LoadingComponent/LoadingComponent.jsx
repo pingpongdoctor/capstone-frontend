4843d318957b7b638ba7c1ad06e4911cb65532e7
Simon Tran <thanhnhantran1501@gmail.com>
2023-06-03 16:13:22 -0700

    fix bugs

--- src/components/LoadingComponent/LoadingComponent.jsx
+++ src/components/LoadingComponent/LoadingComponent.jsx
@@ -5,16 +5,10 @@ import { useEffect } from "react";
 import { useWindowSize } from "../../Utils/utils";
 import { useLocation } from "react-router-dom";
 export default function LoadingComponent({ displayNoneClass }) {
-  //GET CURRENT PATH
-  const currentPath = useLocation().pathname;
   //LOADING PERCENT STATE
   const [loadPercent, setLoadPercent] = useState(0);
   //LOADING COMPONENT SIZE
   const [size, setSize] = useState(80);
-  //SHOW LOADING PERCENT STATE
-  const [showPercent, setShowPercent] = useState(false);
-  //LOADING DOT TEXT
-  const [dotText, setDotText] = useState(".");
   //GET THE CURRENT WIDHT OF THE VIEWPORT
   const currentWidth = useWindowSize().width;
   //USEEFFECT TO SET THE LOADING PERCENT STATE
@@ -32,23 +26,6 @@ export default function LoadingComponent({ displayNoneClass }) {
       setSize(80);
     }
   }, [size]);
-  //SETINTERVAL TO SET THE DOT TEXT STATE
-  setInterval(() => {
-    if (dotText.split("").length < 5) {
-      setDotText(dotText + ".");
-    } else {
-      setDotText(".");
-    }
-  }, 700);
-
-  //USEEFFECT TO SET SHOWPERCENT STATE
-  useEffect(() => {
-    if (currentPath === "/" || currentPath === "/recipe-library") {
-      setShowPercent(true);
-    } else {
-      setShowPercent(false);
-    }
-  }, [currentPath]);
 
   return (
     <div className={`loading-component ${displayNoneClass}`}>
@@ -60,8 +37,6 @@ export default function LoadingComponent({ displayNoneClass }) {
           aria-label="Loading Spinner"
         />
       </div>
-
-      <p className="loading-component__text">{`LOADING${dotText}`}</p>
 
       <p className="loading-component__text">
         LOADING <span>{loadPercent}%</span>
